15f18174c7e54b935bae3232e46f26a107cac2e7
GLOBAL\h459655 <chandushree.ramachandru@honeywell.com>
2025-03-20 14:59:56 +0530

    secondbranch

--- TestJavaConfig/src/samplepackage/SampleProgram.java
+++ TestJavaConfig/src/samplepackage/SampleProgram.java
@@ -5,6 +5,8 @@ public class SampleProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Program Config ready");
+		Sum S = new Sum();
+		S.summing();
 	}
 
 }
---
+++ TestJavaConfig/src/samplepackage/Sum.java
@@ -0,0 +1,9 @@
+package samplepackage;
+
+public class Sum {
+	int a=5, b=6;
+	public void summing() {
+		System.out.printf("sum of a and b is " + (a+b));
+	}
+
+}
